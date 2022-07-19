73ce221ebe57c6a19a5f0e529d930d380ef2d5f1
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-07-20 00:51:53 +0530

    UI Design, Rendering color pallet with Tile.jsx component and Copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Values from "values.js";
+import Tile from "./components/Tile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [val, setVal] = useState("");
+    const [err, setErr] = useState(false);
+    const [errMsg, setErrMsg] = useState("Input cannot be empty");
+    const [clr, setClr] = useState("hsl(112.2, 73.53%, 73.33%)");
+    const color = new Values(clr).all();
+    return (
+        <div className="App">
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder="Color"
+                    className="color"
+                    value={val}
+                    onChange={(e) => {
+                        setErr(false);
+                        setVal(e.target.value);
+                    }}
+                />
+                <small className={err ? "err" : "err hide"}>{errMsg}</small>
+                <button
+                    onClick={(e) => {
+                        if (val.trim() === "") {
+                            setErr(true);
+                            setErrMsg("Input cannot be empty");
+                        } else if (
+                            typeof val === "string" &&
+                            val.length === 6 &&
+                            !isNaN(Number("0x" + val))
+                        ) {
+                            setErr(true);
+                            setErrMsg("Not a valid color");
+                        } else {
+                            setClr(val);
+                            setVal('');
+                            setErr(false);
+                        }
+                    }}
+                >
+                    Generate
+                </button>
+            </div>
+            <section className="colors">
+                {color.map((i, ind) => (
+                    <Tile key={ind} obj={i} />
+                ))}
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Tile.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./tile.css";
+
+function Tile({ obj }) {
+    const [show, setShow] = useState(false);
+    return (
+        <article
+            className="tile"
+            style={{ backgroundColor: `#${obj.hex}` }}
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+            onClick={() => navigator.clipboard.writeText(`#${obj.hex}`)}
+        >
+            <div
+                className={
+                    show
+                        ? obj.type === "tint"
+                            ? "details tint"
+                            : "details shade"
+                        : "details hide"
+                }
+            >
+                <p>{obj.weight}%</p>
+                <p>#{obj.hex}</p>
+                <p>
+                    rgb({obj.rgb[0]}%, {obj.rgb[1]}%, {obj.rgb[2]}%)
+                </p>
+            </div>
+        </article>
+    );
+}
+
+export default Tile;
